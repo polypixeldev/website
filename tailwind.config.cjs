e506878d9db5211e429657ed4c68c311b44d5eef
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-08-29 19:54:09 -0400

    Use space grotesk font for headings and nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Dosis', ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				bgc: '#080404'
-			}
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Dosis", ...defaultTheme.fontFamily.sans],
+        "space-grotesk": [
+          "Space Grotesk",
+          "Dosis",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+      colors: {
+        bgc: "#080404",
+      },
+    },
+  },
+  plugins: [],
+};
