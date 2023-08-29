40d3ffcae004756e6317a04e6a9f741aa3468fc3
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2023-08-29 19:22:21 -0400

    Improve stars

--- src/components/star.tsx
+++ src/components/star.tsx
@@ -1,25 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Star() {
-	const [x, setX] = useState(0);
-	const [y, setY] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
-	useEffect(() => {
-		function handleResize() {
-			setX(Math.floor(Math.random() * window.innerWidth));
-			setY(Math.floor(Math.random() * window.innerHeight));
-		}
+  useEffect(() => {
+    function handleResize() {
+      setX(Math.floor(Math.random() * window.innerWidth));
+      setY(Math.floor(Math.random() * window.innerHeight));
+    }
 
-		window.addEventListener('resize', handleResize);
-		handleResize();
-	}, []);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+  }, []);
 
-	return (
-		<div className={`bg-white rounded-full w-1 h-1 fixed -z-10`} style={{
-			left: x,
-			top: y
-		}}/>
-	);
+  return (
+    <div
+      className="bg-white blur-sm rounded-full w-1 h-1 fixed -z-10"
+      style={{
+        left: x,
+        top: y,
+      }}
+    />
+  );
 }
 
 export default Star;
